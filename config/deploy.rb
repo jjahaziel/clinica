8693964f8a5980a7296e73fc8caa7aa7f7e4ac80
jahaziel Torres <jjahaziel@hotmail.com>
2015-11-04 00:48:28 -0500

    Devise problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, '/home/admin007/clinica_deploy'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml, 'log', 'config/secrets.yml'}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
